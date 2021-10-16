2ed08b9e321313e3079de4e97efb7a319e0b0275
Dana Bottoni <danabottoni@gmail.com>
2021-10-16 18:05:44 -0400

    heroku again

---
+++ client/build/precache-manifest.375c0bf585d0e56f9aa0a32c753439f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bc8fdd7a05905752eab59284d4dd92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0fd3098c9c8f13bc6e3",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "a039f0d4a07f9b2ca125",
+    "url": "/static/js/2.c0653cb1.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.c0653cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0fd3098c9c8f13bc6e3",
+    "url": "/static/js/main.d1c44a31.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
